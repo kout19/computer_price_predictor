e94d3e37a42dcd25d3a40460aeaad149c3cc9723
Kefyalew <koutlook19@gmail.com>
2026-08-02 15:43:43 +0300

    updating env file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -81,7 +81,7 @@ class Settings(BaseSettings):
     )
 
     cors_allow_origins: List[str] = Field(
-        default=["http://localhost:3000", "http://localhost:5173"]
+        default=["http://localhost:3000", "http://localhost:5173", "https://computer-price-predictor.vercel.app"]
     )
 
     model_config = SettingsConfigDict(
